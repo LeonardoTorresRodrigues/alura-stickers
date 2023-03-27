f1c7deb33563b344252ae1fe93c221f0c55521af
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-03-27 12:07:31 -0300

    feat: add parser for json with regex

--- src/App.java
+++ src/App.java
@@ -2,6 +2,8 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -13,10 +15,13 @@ public class App {
         var request = HttpRequest.newBuilder(endereco).GET().build();
         var response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
 
         // pegar só os dados que interessam (título, poster, classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
         // exibir e manipular os dados
+
+        
     }
 }
