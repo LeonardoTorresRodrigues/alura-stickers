3036461b08667863537dca2edb836aee03af89ab
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-03-28 12:06:21 -0300

    refactor: inputStream via URL

--- src/GeradoraDeFigurinhas.java
+++ src/GeradoraDeFigurinhas.java
@@ -3,14 +3,17 @@ import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 
 public class GeradoraDeFigurinhas {
-  public void cria() throws Exception {
+  public void cria(InputStream inputStream, String nomeArquivo) throws Exception {
     
     // leitura da imagem
-    BufferedImage imagemOriginal = ImageIO.read(new File("entrada/filme.jpg"));
+    //InputStream inputStream = new FileInputStream(new File("entrada/filme.jpg"));
+    //InputStream inputStream = new URL("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies_1.jpg").openStream();
+    BufferedImage imagemOriginal = ImageIO.read(inputStream);
 
     // cria nova imagem em memória com transparência e com tamanho novo
     int largura = imagemOriginal.getWidth();
@@ -31,11 +34,6 @@ public class GeradoraDeFigurinhas {
     graphics.drawString("TOPZERA", 100, novaAltura - 100);
   
     // ecrever a nova imagem em um arquivo
-    ImageIO.write(novaImagem, "png", new File("saida/figurinha.png"));
+    ImageIO.write(novaImagem, "png", new File(nomeArquivo));
   }  
-
-  public static void main(String[] args) throws Exception {
-    var geradora = new GeradoraDeFigurinhas();
-    geradora.cria();
-  }
 }
