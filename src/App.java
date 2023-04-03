9e23e2761a05fa89ca215a52a22f50199e9dd5cf
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-04-03 12:44:28 -0300

    refactor: new text with rating

--- src/App.java
+++ src/App.java
@@ -5,15 +5,16 @@ import java.util.List;
 public class App {
     public static void main(String[] args) throws Exception {
         
-        //ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
+        ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
         //String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
 
         //ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
         //String nasaKey = System.getenv("NASA_API_KEY");
         //String url = "https://api.nasa.gov/planetary/apod?api_key=h2IxkhLQNRPvSDk5MmVg5xcH216CSdNIf9YAFaeA&start_date=2022-06-12&end_date=2022-06-14";
 
-        String url = "http://localhost:8080/linguagens";
-        ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
+        //String url = "http://localhost:8080/linguagens";
+        //ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
         
         var http = new ClienteHttp();
         String json = http.buscaDados(url);
@@ -28,12 +29,20 @@ public class App {
 
             InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
             String nomeArquivo = conteudo.getTitulo();
+            double classificacao = conteudo.getClassificacao();
 
-            geradora.cria(inputStream, nomeArquivo);
+            String textoFigurinha;
+            if (classificacao >= 8.0) {
+                textoFigurinha = "TOP " + classificacao  + "/10";
+            } else {
+                textoFigurinha = "MEH " + classificacao  + "/10";
+            }
+
+            geradora.cria(inputStream, nomeArquivo, textoFigurinha);
 
             //System.out.println("\u001b[1m" + titulo + "\u001b[m");
             //System.out.println("\u2B50 " + nota + "/10");
-            System.out.println(conteudo.getTitulo());
+            System.out.println(nomeArquivo);
             System.out.println();
         }
     }
